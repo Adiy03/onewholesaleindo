e6a53062f205cf8d2a007db2c30bfe2942bf9542
Adiy03 <adiyogo3@gmail.com>
2025-05-26 21:34:54 +0700

    add wa button

--- assets/js/produk.js
+++ assets/js/produk.js
@@ -2,10 +2,10 @@ const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
   const storeitems = document.getElementById("product-list");
   const product = document.querySelectorAll(".produk");
-  const pname = storeitems.getElementsByTagName("h1");
+  const pname = storeitems.getElementsByTagName("h2");
 
   for(var i = 0; i < pname.length; i++){
-    let match = product[i].getElementsByTagName('h1')[0];
+    let match = product[i].getElementsByTagName('h2')[0];
 
     if (match){
       let textvalue = match.textContent || match.innerHTML
